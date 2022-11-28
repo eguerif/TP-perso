4a747ef8adaa0f49a193c3ccffe6c2445528e2cd
eguerif <90249172+eguerif@users.noreply.github.com>
2022-11-28 16:31:34 +0100

    Add files via upload

---
+++ POOkedex/mainwindow.h
@@ -0,0 +1,49 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QNetworkAccessManager>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    void getPokemon(int);
+    void getPokePrecedent(int);
+    void getImg(QString);
+
+private slots:
+    void managePokemonReply();
+    void manageImgReply();
+    void on_Validation_clicked();
+
+    void on_PokeP1_clicked();
+
+    void on_PokeM1_clicked();
+
+    void on_PokeP10_clicked();
+
+    void on_PokeM10_clicked();
+
+    void on_PokePrecedent_clicked();
+
+    void on_lineEdit_returnPressed();
+
+private:
+    Ui::MainWindow *ui;
+    QNetworkAccessManager *manager;
+    QNetworkReply *pokemonReply;
+    QNetworkReply *imgReply;
+    QStack<int> *Precedentpoke;
+    int id;
+
+};
+
+#endif // MAINWINDOW_H
